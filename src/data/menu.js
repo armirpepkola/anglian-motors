4c459cc99c8d9da7c6039a0c322e866e228f3744
armirpepkola <armirpepkola@gmail.com>
2025-07-20 18:36:12 +0100

    first fix

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,21 +1,11 @@
 export const menuItems = [
     {
         title: "Home",
-        icon: "fas fa-chevron-down",
-        subMenu: [
-            { title: "Home 01", link: "/" },
-            { title: "Home 02", link: "/home-2" },
-            { title: "Home 03", link: "/home-3" },
-            { title: "Home 04", link: "/home-4" },
-        ],
+        link: "/",
     },
     {
         title: "About Us",
-        icon: "fas fa-chevron-down",
-        subMenu: [
-            { title: "About One", link: "/about" },
-            { title: "About Two", link: "/about-2" },
-        ],
+        link: "/about",
     },
     {
         title: "Services",
@@ -26,51 +16,7 @@ export const menuItems = [
         ],
     },
     {
-        title: "Pages",
-        icon: "fas fa-chevron-down",
-        subMenu: [
-            { title: "Our Project 01", link: "/project" },
-            { title: "Our Project 02", link: "/project-2" },
-            { title: "Project Details", link: "/project/single" },
-            { title: "Our Teams", link: "/team" },
-            { title: "Our Teams Details", link: "/team/single" },
-            { title: "FAQ", link: "/faq" },
-            { title: "Login / Register", link: "/login" },
-            { title: "404 Error", link: "/error" },
-        ],
+        title: "Contact Us",
+        link: "/contact",
     },
-    {
-        title: "Blog",
-        icon: "fas fa-chevron-down",
-        subMenu: [
-            {
-                title: "Blog Grid",
-                subMenu: [
-                    { title: "2 Columns", link: "/blog-2" },
-                    { title: "3 Columns", link: "/blog-3" },
-                ],
-            },
-            { title: "Blog Grid One", link: "/blog-2" },
-            { title: "Blog Grid Two", link: "/blog-3" },
-            {
-                title: "Blog Standard",
-                subMenu: [
-                    { title: "Left SideBar", link: "/blog-left" },
-                    { title: "Right SideBar", link: "/blog" },
-                ],
-            },
-            { title: "Blog Standard One", link: "/blog" },
-            { title: "Blog Standard Two", link: "/blog-left" },
-            {
-                title: "Blog Details",
-                subMenu: [
-                    { title: "Left SideBar", link: "/blog-single-left" },
-                    { title: "Right SideBar", link: "/blog/single" },
-                ],
-            },
-            { title: "Blog Details", link: "/blog/single" },
-
-        ],
-    },
-    { title: "Contact Us", link: "/contact" },
 ];
